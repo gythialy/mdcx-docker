0a8ef752de51e57a90f513ab111e3ef169682d76
gythialy <gythialy.koo+github@gmail.com>
2023-02-12 23:49:33 +0800

    fix: app version and permission

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
 FROM ghcr.io/gythialy/mdcx-base:latest
 
-ENV APP_NAME        "Mdcx"
-ENV USER_ID         0
-ENV GROUP_ID        0
-ENV ENABLE_CJK_FONT 1
-ENV LANG            zh_CN.UTF-8
-ENV TZ              Asia/Shanghai
+# Set environment variables.
+ENV APP_NAME="MDCX" \
+    USER_ID=0 \
+    GROUP_ID=0 \
+    LANG=zh_CN.UTF-8 \
+    TZ=Asia/Shanghai
 
 ADD app /app
 COPY config.sample.ini /app/
+COPY 11-app-init.sh /etc/cont-init.d/
 
 WORKDIR /app
 
 EXPOSE 5800 5900
 
-RUN python3.9 -m pip install --no-cache-dir -r requirements.txt 
+RUN python3.9 -m pip install --no-cache-dir -r requirements.txt && \
     # -i https://pypi.douban.com/simple
+    set-cont-env APP_VERSION "$(cat /app/version)" && \
+    set-cont-env DOCKER_IMAGE_VERSION "v1.0.$(cat /app/version)"
 
 COPY startapp.sh /startapp.sh
--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x;
+set -x
 
 LIBS=(jq curl unrar find)
 MDCX_FILE="mdcx.rar"
@@ -21,28 +21,26 @@ for b in "${LIBS[@]}"; do
   fi
 done
 
-assets_url=$(curl -s https://api.github.com/repos/anyabc/something/releases/latest | jq -r '.assets[].browser_download_url' | grep $MDCX_PREFIX)
+ASSET_URL=$(curl -sSL https://api.github.com/repos/anyabc/something/releases/latest | jq -r '.assets[].browser_download_url' | grep $MDCX_PREFIX)
 
-echo "downloading $MDCX_FILE from $assets_url"
+echo "downloading $MDCX_FILE from $ASSET_URL"
 
-curl -L "$assets_url" -o $MDCX_FILE && unrar x -y -inul $MDCX_FILE
+curl -L "$ASSET_URL" -o $MDCX_FILE && unrar x -y -inul $MDCX_FILE
 
-extract_file="$(find . -type d -iname "${MDCX_PREFIX}*")" && echo "file: $extract_file"
+EXTRACT_FILE="$(find . -type d -iname "${MDCX_PREFIX}*")" && echo "file: $EXTRACT_FILE"
 
-mv "${extract_file}" app && ls -al
+mv "${EXTRACT_FILE}" app && ls -al
 
 [[ ! -e "${VERSION_FILE}" ]] || touch "${VERSION_FILE}"
 
-MDCX_OLD_VERSION="$(cat version)"
-MDCX_VERSION="$(echo "$extract_file" | cut -d'-' -f 3)"
-echo -e "${MDCX_VERSION}" > "${VERSION_FILE}"
-echo -e "${MDCX_VERSION}" > "version"
+MDCX_VERSION="$(echo "$EXTRACT_FILE" | cut -d'-' -f 3)"
+echo -e "${MDCX_VERSION}" >"${VERSION_FILE}"
+echo -e "${MDCX_VERSION}" >"version"
 
-is_changed='false'
-if [[ "$(cat version)" != "$(cat ${VERSION_FILE})" ]];then
-  is_changed='true'
-fi
-echo "is_changed=${is_changed}" >> $GITHUB_OUTPUT
-echo "tag=v1.0.${MDCX_VERSION}" >> $GITHUB_OUTPUT
+git diff --quiet --exit-code
+[[ $? -ne 0 ]] && is_changed='true' || is_changed='false'
+
+echo "is_changed=${is_changed}" >>$GITHUB_OUTPUT
+echo "tag=v1.0.${MDCX_VERSION}" >>$GITHUB_OUTPUT
 
 rm -rf ${MDCX_FILE}
